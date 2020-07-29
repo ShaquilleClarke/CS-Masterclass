d57fbc432f7b7008f72b106eb857b69a80c8f19e
ShaquilleClarke <github email address>
2020-07-28 20:59:02 -0400

    thoughts

--- ProblemSolving/charCount.js
+++ ProblemSolving/charCount.js
@@ -4,10 +4,23 @@ const charCount = (str) => {
     const cache = {};
 
     for (let char of str.toLowerCase()) {
-        !cache[char] ? cache[char] = 1 : cache[char]++; 
+        if(isAlphaNumeric(char)) {
+            cache[char] = ++cache[char] || 1; 
+        }
     }
-
     return cache
 }
 
-console.log(charCount("Apple"))
+const isAlphaNumeric = (char) => {
+    const code = char.charCodeAt(0);
+    if(!(code > 47 && code < 58) && // numeric (0-9)
+       !(code > 64 && code < 91) && // upper Alphabet (A-Z)
+       !(code > 96 && code < 123)) { // lower alpha (a-z)
+        return false
+    }
+    return true
+}
+
+
+
+console.log(charCount("Apple was here"))
