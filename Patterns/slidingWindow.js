67e6099c97ca1489d4bc2bad1ee5d0cc343460ec
ShaquilleClarke <github email address>
2020-08-23 16:18:29 -0400

    update

--- Patterns/slidingWindow.js
+++ Patterns/slidingWindow.js
@@ -7,23 +7,72 @@ const assert = require('assert').strict;
 // Write a function called maxSubArraySum which accepts an array of integers and a number called n. This 
 // function should calculate the maximum sum of n consecutive elements in the array
 
-const maxSubArraySum = (arr, n) => {
-    let tempSum = 0, maxSum = 0;
+// const maxSubArraySum = (arr, n) => {
+//     let tempSum = 0, maxSum = 0;
 
-    for (let i = 0; i < n; i++) {
-        maxSum += arr[i];
+//     for (let i = 0; i < n; i++) {
+//         maxSum += arr[i];
+//     }
+//     tempSum = maxSum; 
+//     for (let i = n; i < arr.length; i++) {
+//         tempSum = tempSum - arr[i - n] + arr[i];
+//         // console.log(tempSum)
+//         maxSum = Math.max(maxSum, tempSum);
+//         // console.log('maxSum is:', maxSum)
+//     }
+//     return maxSum;
+// }
+
+//assert.deepStrictEqual(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2), 10)
+//onsole.log('result: ', maxSubArraySum([100, 200, 300, 400], 2))
+
+//=============Alternate Take================
+
+// Given an array of integers and a number, write a function which finds the maximum sum of a subarray
+// with the length of the number passed into the function
+
+// The subarray must consist of consecutive elements from the original array.
+
+// const maxSubArraySum = (arr, n) => {
+//     if (arr.length < n) return null;
+
+//     let total = 0, currentTotal = 0;
+
+//     for (let i = 0; i < n; i++) {
+//         total += arr[i];
+//     }
+//     currentTotal = total;
+//     for (i = n; i < arr.length; i++) {
+//         currentTotal += arr[i] - arr[i - n];
+//         total = Math.max(total, currentTotal);
+//     }
+//     return total;
+// }
+// console.log(maxSubArraySum([100, 200, 300, 400], 2))
+
+//=================================================================
+
+const minSubArrayLen = (arr, target) => {
+    let wndwStart = 0, tempSum = 0, minSet = Number.MAX_VALUE;
+    
+    for (let wndwEnd = 0; wndwEnd < arr.length; wndwEnd++) {
+        tempSum += arr[wndwEnd];
+        while (tempSum >= target) {
+            // if (wndwEnd === arr.length) return 0;
+            minSet = Math.min(minSet, wndwEnd - wndwStart + 1);
+            tempSum -= wndwStart;
+            wndwStart++;
+        }
     }
-    tempSum = maxSum; 
-    for (let i = n; i < arr.length; i++) {
-        tempSum = tempSum - arr[i - n] + arr[i];
-        // console.log(tempSum)
-        maxSum = Math.max(maxSum, tempSum);
-        // console.log('maxSum is:', maxSum)
-    }
-    return maxSum;
+    // return minSet
+    console.log(minSet)
 }
-
-assert.deepStrictEqual(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2), 10)
-console.log('result: ', maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2))
+// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52))
+//console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
+//console.log(minSubArrayLen([2, 1, 6, 5, 4], 9))
+//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39))
+//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55))
+//console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11))
+//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95))
 
 
