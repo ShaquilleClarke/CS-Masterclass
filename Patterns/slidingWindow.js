e65b778dfcadb7b026645cc74b1ad86678ac2a1a
ShaquilleClarke <github email address>
2020-09-05 17:19:40 -0400

    brushing up on sliding windows

--- Patterns/slidingWindow.js
+++ Patterns/slidingWindow.js
@@ -52,27 +52,28 @@ const assert = require('assert').strict;
 
 //=================================================================
 
-const minSubArrayLen = (arr, target) => {
-    let wndwStart = 0, tempSum = 0, minSet = Number.MAX_VALUE;
-    
-    for (let wndwEnd = 0; wndwEnd < arr.length; wndwEnd++) {
-        tempSum += arr[wndwEnd];
-        while (tempSum >= target) {
-            // if (wndwEnd === arr.length) return 0;
-            minSet = Math.min(minSet, wndwEnd - wndwStart + 1);
-            tempSum -= wndwStart;
-            wndwStart++;
+// Write a function that accepts two parameters, an array of positive integers and positive number.
+// The function should return the minimal length of a contiguous subarray of the which the sum is greater than
+// or equal to the number passed in to the function. If there isn't one, return 0 
+
+const minSubArrayLen = (arr, sum) => {
+    let end = 0, total = 0, start = 0, minLen = Infinity;
+
+    while (start < arr.length) {
+        if (total < sum && end < arr.length) {
+            total += arr[end];
+            end++
         }
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start);
+            total -= arr[start];
+            start++;
+        }
+        else break
     }
-    // return minSet
-    console.log(minSet)
+    return minLen === Infinity ? 0 : minLen;
 }
-// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52))
-//console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7))
-//console.log(minSubArrayLen([2, 1, 6, 5, 4], 9))
-//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39))
-//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55))
-//console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11))
-//console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95))
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9))
+
 
 
