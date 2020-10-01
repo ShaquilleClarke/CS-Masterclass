98fa6d6788039637b596aa8f02ca665fef870259
ShaquilleClarke <github email address>
2020-10-01 10:56:33 -0400

    allow me to explain twoSum

--- Data Structures/Hashes/twoSum.js
+++ Data Structures/Hashes/twoSum.js
@@ -9,3 +9,8 @@ const twoSum = (nums, target) => {
     }
 }
 console.log(twoSum([4, 2, 4], 8))
+
+// We must iterate through the array, subtract the current element from the target,
+// then check to see if the difference is located within the map. If it is, we'll return an array
+// containing the location of the difference within the map along with the current index of the array.
+// Otherwise, we will store the arrays current index within the map and set its value to the current index.
