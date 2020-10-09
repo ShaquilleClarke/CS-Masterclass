f034a42e43112a41b97232a94ca47caf9840ba7a
ShaquilleClarke <github email address>
2020-10-09 14:50:19 -0400

    linked list - shift, unshift, pop, get

--- Data Structures/LinkedList/singlyLinkedList.js
+++ Data Structures/LinkedList/singlyLinkedList.js
@@ -28,6 +28,55 @@ class LinkedList {
         return this;
 
     }
+
+    pop() {
+        if (!this.head) return;
+
+        let prev = this.head, next = prev.next;
+
+        while (next.next !== null) {
+            prev = prev.next;
+            next = next.next
+        }
+        prev.next = null;
+        this.length--;
+        this.tail = prev;
+        return next;
+    }
+
+    shift() {
+        if (!this.head) return;
+        this.head = this.head.next;
+        this.length--;
+    }
+
+    unShift(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head
+        }
+        newNode.next = this.head; 
+        this.head = newNode
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (!this.head) return;
+
+        if(index === 0) return this.head;
+
+        let current = this.head, indx = 0;
+
+        while(current) {
+            if(indx === index) return current;
+            current = current.next;
+            indx++;
+        }
+        return null;
+
+    }
 }
 
 let list = new LinkedList;
@@ -36,9 +85,15 @@ list.push('dumpty');
 list.push('butt cheeks');
 list.push('keeo pushin');
 list.push('abracadabra')
-log(list.head);
-// log(list.head.next);
-// log(list.head.next.next);
-// log(list.head.next.next.next);
-// log(list.head.next.next.next.next);
+log(list.pop());
+log(list)
+log(list.pop());
+log(list)
+log(list.shift());
+log(list)
+log(list.unShift('coochie'))
+log(list.get(2))
+
+// log(list.pop());
+// log(list.pop());
 
