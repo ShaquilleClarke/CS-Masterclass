c43495cbd91d40faf1a843389a3d4fb4f24e5610
ShaquilleClarke <github email address>
2020-10-22 13:40:07 -0400

    doubly linked list - shift, unshift, and get methods complete

--- Data Structures/LinkedList/doublyLinkedList.js
+++ Data Structures/LinkedList/doublyLinkedList.js
@@ -43,11 +43,55 @@ class DoublyLinkedList {
         this.length--;
         return poppedNode;
     }
+
+    shift() {
+        if (!this.head) return;
+        let removed = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = removed.next;
+            this.head.prev = null;
+        }
+        this.length--;
+        return removed;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (!this.head || index < 0) return;
+        if (index === 0) return this.head;
+        let current = this.head, count = 0;
+        while (current) {
+            if (index === count) return current;
+            current = current.next;
+            count++;
+        }
+        return false;
+    }
 }
+
+
 
 let list = new DoublyLinkedList;
 list.push(1);
 list.push(2);
-list.pop();
+list.shift();
+list.unshift(1);
+list.unshift(6);
+list.unshift(8);
 // list.pop();
-log(list.tail)
+log(list.get(3))
