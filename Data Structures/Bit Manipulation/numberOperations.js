3e3f2472ea411d5a4dc3a09caf59a6cd4f8e0c97
ShaquilleClarke <github email address>
2020-12-04 15:36:36 -0500

    bit manipulation: multiplyBit function complete

--- Data Structures/Bit Manipulation/numberOperations.js
+++ Data Structures/Bit Manipulation/numberOperations.js
@@ -18,5 +18,20 @@ const subtractBit = (a, b) => {
     return addBit(a, negateBit(b))
 }
 
-log(subtractBit(7, 3).toString(2))
+const multiplyBit = (a, b) => {
+    let m = 1, c = 0;
+    if(a < 0) {
+        a = negateBit(a);
+        b = negateBit(b);
+    }
+    while (a >= m && b) {
+        if(a & m) c = addBit(b, c);
+        b = b << 1;
+        m = m << 1;
+    }
+    return c;
+}
+
+// log(subtractBit(7, 3).toString(2))
 // log(addBit(9, 5).toString(2))
+log(multiplyBit(4, 5).toString(2))
