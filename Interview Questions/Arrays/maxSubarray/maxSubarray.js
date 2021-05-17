5a6ec976ffc7c37457914f99d54c9e46954076a3
ShaquilleClarke <github email address>
2021-05-17 14:07:36 -0400

    maxSubArray - solved with Kadanes Algorithm

--- Interview Questions/Arrays/maxSubarray/maxSubarray.js
+++ Interview Questions/Arrays/maxSubarray/maxSubarray.js
@@ -1,14 +1,12 @@
 const log = console.log;
 
 const maxSubArray = (nums) => {
-    let max = 0, sum = 0, i = 0;
+    let max = nums[0], sum = nums[0], i = 1;
     while (i < nums.length) {
-        if (sum <= 0) sum = 0;
-        else if (sum <= 0 && nums[i-1] >=0) sum += nums[i-1];
-        sum += nums[i];
+        sum = Math.max(nums[i], nums[i] + sum);
         max = Math.max(max, sum);
         i++
     }
     return max;
 }
-log(maxSubArray([5,4,-1,7,8]))
+log(maxSubArray([-1]))
