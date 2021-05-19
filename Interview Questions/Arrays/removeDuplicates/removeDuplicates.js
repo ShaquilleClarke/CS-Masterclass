75a7bd28e46b7c8843ce4960550ed627bf6390f4
ShaquilleClarke <github email address>
2021-05-19 15:49:15 -0400

    removeDuplicates - solved

---
+++ Interview Questions/Arrays/removeDuplicates/removeDuplicates.js
@@ -0,0 +1,14 @@
+const log = console.log;
+
+const removeDuplicates = (nums) => {
+    let i = 1;
+    while (i < nums.length) {
+        if (nums[i] === nums[i-1]) {
+            nums.splice(i-1, 1);
+            i--;
+        }
+        i++;
+    }
+    return i;
+}
+log(removeDuplicates([0, 0, 1, 1, 1, 2, 3, 3, 4]))
