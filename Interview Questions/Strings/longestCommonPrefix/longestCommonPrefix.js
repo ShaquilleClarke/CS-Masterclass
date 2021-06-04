4ce5a0bbaa31911eb88425b372b7b5ab59850ac8
ShaquilleClarke <github email address>
2021-06-04 14:10:51 -0400

    longestCommonPrefix - solved and optimized

--- Interview Questions/Strings/longestCommonPrefix/longestCommonPrefix.js
+++ Interview Questions/Strings/longestCommonPrefix/longestCommonPrefix.js
@@ -2,12 +2,20 @@ const log = console.log;
 
 const longestCommonPrefix = (strs) => {
     if (!strs.length) return "";
-    const firstWrd = strs[0];
-    for (let i = 0; i < firstWrd.length; i++) {
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== firstWrd[i]) return strs[j].slice(0, i)
+    if (strs.length === 1) return strs[0];
+    let frstWrd = strs[0], len = frstWrd.length, currStr;
+    while (len > 0) {
+        for (let i = 1; i < strs.length; i++) {
+            currStr = strs[i].substr(0, len);
+            if (currStr !== frstWrd) {
+                break;
+            } else {
+                if (i === strs.length-1) return frstWrd.substr(0, len);
+            }
         }
+        len--;
+        frstWrd = frstWrd.substr(0, len);
     }
-    return strs[0];
+    return '';
 }
-log(longestCommonPrefix(["flower", "flow", "flows"]))
+log(longestCommonPrefix(['flow', 'flight', 'flowers']))
