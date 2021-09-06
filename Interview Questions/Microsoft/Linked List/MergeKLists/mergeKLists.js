1ffa68f38f2d2a4034189bd0273ab8c5c87b619f
ShaquilleClarke <github email address>
2021-09-06 15:51:28 -0400

    mergeKLists - complete

--- Interview Questions/Microsoft/Linked List/MergeKLists/mergeKLists.js
+++ Interview Questions/Microsoft/Linked List/MergeKLists/mergeKLists.js
@@ -19,6 +19,8 @@
 
 // Input: lists = []
 // Output: []
+
+
 // Example 3:
 
 // Input: lists = [[]]
@@ -27,13 +29,47 @@
 
 //   Definition for singly-linked list.
 class ListNode {
-    constructor() {
+    constructor(val) {
       this.val = val;
-      this.next = next;
+      this.next = null;
     }
 }
 
+// function ListNode(val, next) {
+//   this.val = (val===undefined ? 0 : val)
+//   this.next = (next===undefined ? null : next)
+// }
+
+const log = console.log;
 
 const mergeKLists = (lists) => {
-    
+  if (!lists.length) return null;
+  else if (lists.length === 1) return lists[0];
+  
+  let mid = (lists.length + 1) / 2, 
+  left = lists.slice(0, mid), 
+  right = lists.slice(mid), 
+  divL = mergeKLists(left),
+  divR = mergeKLists(right);
+
+  return merged(divL, divR)
 };
+
+const merged = (l1, l2) => {
+  let dummy = new ListNode(0), curr = dummy;
+  
+  while (l1 && l2) {
+      if (l1.val < l2.val) {
+          curr.next = l1;
+          l1 = l1.next;
+      } else {
+          curr.next = l2;
+          l2 = l2.next;
+      }
+      curr = curr.next;
+  }
+  curr.next = l1 || l2;
+  return dummy.next;
+}
+
+log(mergeKLists([[1,8,11],[5, 9, 10],[3,4,7]]))
