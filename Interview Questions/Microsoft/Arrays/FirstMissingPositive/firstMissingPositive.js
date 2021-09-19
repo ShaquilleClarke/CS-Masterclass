3909317d49b7745b6d8e219ada9e0b47957d18b7
ShaquilleClarke <github email address>
2021-09-19 14:24:42 -0400

    Arrays - firstMissingPositive - solved

--- Interview Questions/Microsoft/Arrays/FirstMissingPositive/firstMissingPositive.js
+++ Interview Questions/Microsoft/Arrays/FirstMissingPositive/firstMissingPositive.js
@@ -8,6 +8,7 @@
 
 // Input: nums = [1,2,0]
 // Output: 3
+
 // Example 2:
 
 // Input: nums = [3,4,-1,1]
@@ -17,6 +18,24 @@
 // Input: nums = [7,8,9,11,12]
 // Output: 1
 
+const log = console.log;
+
 const firstMissingPositive = (nums) => {
+    let i = 0;
     
+    while (i < nums.length) {
+        if (
+            nums[i] > 0 &&
+            nums[i] <= nums.length &&
+            nums[nums[i]-1] !== nums[i]
+        ) [nums[nums[i]-1], nums[i]] = [nums[i], nums[nums[i]-1]];
+        else i++;
+    }
+
+    for ( let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i+1) return i+1;
+    }
+    return i+1;
 };
+
+log(firstMissingPositive([3,4,-1,1]))
