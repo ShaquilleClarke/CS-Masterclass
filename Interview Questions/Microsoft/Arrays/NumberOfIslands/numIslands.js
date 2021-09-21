14fa0a942dcefe9756a523c5ac51105213b44bb6
ShaquilleClarke <github email address>
2021-09-21 19:13:29 -0400

    Arrays: numIslands - completed

--- Interview Questions/Microsoft/Arrays/NumberOfIslands/numIslands.js
+++ Interview Questions/Microsoft/Arrays/NumberOfIslands/numIslands.js
@@ -25,6 +25,51 @@
 // ]
 // Output: 3
 
+const log = console.log;
+
+
 const numIslands = (grid) => {
-    
+    let num = 0;
+    for (let row = 0; row <= grid.length-1; row++) {
+        for (let col = 0; col <= grid[0].length-1; col++) {
+            if(grid[row][col] === "1") {
+                num++;
+                traverse(grid, row, col)
+            }
+       }
+    }
+    return num;
 };
+
+const traverse = (grid, row, col) => {
+    // Make sure row and column are in bounds
+    let rowInbounds = row < grid.length && row >= 0,
+    colInbounds = col < grid[0].length && col >= 0;
+
+    if (
+        !colInbounds || 
+        !rowInbounds ||
+        grid[row][col] === "0"
+    ) return false  
+
+    grid[row][col] = "0";
+    // traverse(up)
+    traverse(grid, row-1, col)
+    // traverse(down)
+    traverse(grid, row+1, col)
+    // traverse(left)
+    traverse(grid, row, col-1)
+    // traverse(right)
+    traverse(grid, row, col+1)
+    return true
+    
+}
+
+const grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+];
+
+log(numIslands(grid))
